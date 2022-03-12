c97652b32132f3af418deb367e6358180effedcb
takahiro.tominaga <tt.300607@gmail.com>
2022-03-12 20:09:21 +0900

    ImagePicker !!!!

--- TwitterClone/Core/Authentication/Views/ProfilePhotoSelectorView.swift
+++ TwitterClone/Core/Authentication/Views/ProfilePhotoSelectorView.swift
@@ -8,26 +8,54 @@
 import SwiftUI
 
 struct ProfilePhotoSelectorView: View {
+    
+    @State private var showImagePicker = false
+    @State private var selectedImage: UIImage?  // UIKit
+    @State private var profileImage: Image?     // SwiftUI
+    
     var body: some View {
         VStack {
             AuthHeaderView(title1: "Setup account",
                            title2: "Add a profile photo")
             
             Button {
-                print("Pick image here..")
+                showImagePicker.toggle()
             } label: {
-                Image("plus_photo")
-                    .resizable()
-//                    .renderingMode(.template)
-                    .scaledToFit()
-                    .frame(width: 180, height: 180)
-                    .padding(.top, 44)
+                if let profileImage = profileImage {
+                    profileImage
+                        .resizable()
+                        .modifier(ProfileImageModifier())
+                } else {
+                    Image("plus_photo")
+                        .resizable()
+                        .modifier(ProfileImageModifier())
+                }
             }
-
+            .sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
+                ImagePicker(selectedImage: $selectedImage)
+            }
+            .padding(.top, 44)
             
             Spacer()
         }
         .ignoresSafeArea()
+    }
+    
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        // convert uikit image to swiftui image
+        profileImage = Image(uiImage: selectedImage)
+    }
+}
+
+// custom modifier
+private struct ProfileImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+    //                    .renderingMode(.template)
+            .scaledToFit()
+            .frame(width: 180, height: 180)
+            .clipShape(Circle())
     }
 }
 
