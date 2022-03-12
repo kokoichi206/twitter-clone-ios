f2e4f952bd467ba2fa1f66691f5115231e39c6d5
takahiro.tominaga <tt.300607@gmail.com>
2022-03-12 15:22:41 +0900

    top profile ui

---
+++ TwitterClone/Core/Profile/Views/ProfileView.swift
@@ -0,0 +1,138 @@
+//
+//  ProfileView.swift
+//  TwitterClone
+//
+//  Created by Takahiro Tominaga on 2022/03/12.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            headerView
+            
+            actionButtons
+            
+            userInfoDetails
+            
+            Spacer()
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
+
+extension ProfileView {
+    
+    var headerView: some View {
+        ZStack(alignment: .bottomLeading) {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            
+            VStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 20, height: 16)
+                        .foregroundColor(.white)
+                        .offset(x: 16, y: 12)
+                }
+
+                Circle()
+                    .frame(width: 72, height: 72)
+                    .offset(x: 16, y: 24)
+            }
+        }
+        .frame(height:96)
+    }
+    
+    var actionButtons: some View {
+        HStack(spacing: 12) {
+            Spacer()
+            
+            Image(systemName: "bell.badge")
+                .font(.title3)
+                .padding(6)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
+            
+            Button {
+                
+            } label: {
+                Text("Edit Profile")
+                    .font(.subheadline).bold()
+                    .frame(width: 120, height: 32)
+                    .foregroundColor(.black)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 0.75))
+            }
+
+        }
+        .padding(.trailing)
+    }
+    
+    var userInfoDetails: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Text("Kokoichi")
+                    .font(.title2).bold()
+                
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundColor(Color(.systemBlue))
+            }
+            
+            Text("@foobar")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Text("This is my bio")
+                .font(.subheadline)
+                .padding(.vertical)
+            
+            HStack(spacing: 24) {
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                    
+                    Text("TOKYO, JP")
+                }
+                                    
+                HStack {
+                    Image(systemName: "link")
+                    
+                    Text("www.thejoker.com")
+                }
+            }
+            .font(.caption)
+            .foregroundColor(.gray)
+            
+            HStack(spacing: 24) {
+                HStack(spacing: 4) {
+                    Text("777")
+                        .font(.subheadline)
+                        .bold()
+                    
+                    Text("Following")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                                    
+                HStack(spacing: 4) {
+                    Text("12.4M")
+                        .font(.subheadline)
+                        .bold()
+                    
+                    Text("Followers")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.vertical)
+        }
+        .padding(.horizontal)
+    }
+}
