31e4d76fdba7cdf3205c4113444de1215bee3e6c
takahiro.tominaga <tt.300607@gmail.com>
2022-03-12 15:45:38 +0900

    explore view

---
+++ TwitterClone/Core/Components/Users/UserRowView.swift
@@ -0,0 +1,37 @@
+//
+//  UserRowView.swift
+//  TwitterClone
+//
+//  Created by Takahiro Tominaga on 2022/03/12.
+//
+
+import SwiftUI
+
+struct UserRowView: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            Circle()
+                .frame(width: 48, height: 48)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("joker")
+                    .font(.subheadline).bold()
+                    .foregroundColor(.black)
+                
+                Text("koko")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+struct UserRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserRowView()
+    }
+}
