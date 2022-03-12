3ad029940d7f4c405410fe84333278e533a47830
takahiro.tominaga <tt.300607@gmail.com>
2022-03-12 17:14:50 +0900

    Login view

---
+++ TwitterClone/Core/Components/RoundedShape.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedShape.swift
+//  TwitterClone
+//
+//  Created by Takahiro Tominaga on 2022/03/12.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        
+        return Path(path.cgPath)
+    }
+}
