e5b6eabc3bcd151d98ae1c1d144e45f6480c3962
takahiro.tominaga <tt.300607@gmail.com>
2022-03-12 16:49:17 +0900

    new tweet page

---
+++ TwitterClone/Core/UploadTweet/Views/NewTweetView.swift
@@ -0,0 +1,57 @@
+//
+//  NewTweetView.swift
+//  TwitterClone
+//
+//  Created by Takahiro Tominaga on 2022/03/12.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    
+    @State private var caption = ""
+    // 全画面表示をコントロールするもの
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(.systemBlue))
+                }
+
+                Spacer()
+                
+                Button {
+                    print("Tweet")
+                } label: {
+                    Text("Tweet")
+                        .bold()
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+            }
+            .padding()
+            
+            HStack(alignment: .top) {
+                Circle()
+                    .frame(width: 64, height: 64)
+                
+                TextArea("What's happening?", text: $caption)
+            }
+            .padding()
+        }
+    }
+}
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
