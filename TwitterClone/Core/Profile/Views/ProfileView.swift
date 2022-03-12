b76737dd2ed050ee7ed517918a4f5bd2f0033b70
takahiro.tominaga <tt.300607@gmail.com>
2022-03-12 16:00:56 +0900

    ui sidemenu

--- TwitterClone/Core/Profile/Views/ProfileView.swift
+++ TwitterClone/Core/Profile/Views/ProfileView.swift
@@ -119,28 +119,8 @@ extension ProfileView {
             .font(.caption)
             .foregroundColor(.gray)
             
-            HStack(spacing: 24) {
-                HStack(spacing: 4) {
-                    Text("777")
-                        .font(.subheadline)
-                        .bold()
-                    
-                    Text("Following")
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                }
-                                    
-                HStack(spacing: 4) {
-                    Text("12.4M")
-                        .font(.subheadline)
-                        .bold()
-                    
-                    Text("Followers")
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                }
-            }
-            .padding(.vertical)
+            UserStatsView()
+                .padding(.vertical)
         }
         .padding(.horizontal)
     }
---
+++ TwitterClone/Core/SideMenu/Views/SideMenuView.swift
@@ -0,0 +1,57 @@
+//
+//  SideMenuView.swift
+//  TwitterClone
+//
+//  Created by Takahiro Tominaga on 2022/03/12.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("kokoichi")
+                        .font(.headline)
+                    
+                    Text("@koko")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                
+                UserStatsView()
+                    .padding(.vertical)
+            }
+            .padding(.leading)
+            
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { option in
+                HStack(spacing: 16) {
+                    Image(systemName: option.imageName)
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    
+                    Text(option.title)
+                        .font(.subheadline)
+                    
+                    Spacer()
+                }
+                .frame(height: 40)
+                .padding(.horizontal)
+            }
+            
+            // Move TOP!!
+            Spacer()
+        }
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
