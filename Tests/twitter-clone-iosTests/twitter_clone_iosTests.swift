dcb21f67521d8a3b5ead146b97af47f283ac5c35
takahiro.tominaga <tt.300607@gmail.com>
2022-03-13 10:13:08 +0900

    fix: build problem (#1)

---
+++ Tests/twitter-clone-iosTests/twitter_clone_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import twitter_clone_ios
+
+final class twitter_clone_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(twitter_clone_ios().text, "Hello, World!")
+    }
+}
