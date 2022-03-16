6f59c3c14e08bb7f83192ebeadba044bda458cad
takahiro.tominaga <tt.300607@gmail.com>
2022-03-16 20:09:33 +0900

    accept specific version as a script option (#8)

--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -22,6 +22,61 @@ IS_MINOR_UPDATE=false
 # ==================== ====== ====================
 
 PROGRAM="$(basename "$0")"
+NEW_VERSION=""
+
+function print_error() {
+    ERROR='\033[1;31m'
+    NORMAL='\033[0m'
+    echo -e "${ERROR}ERROR${NORMAL}: $1"    
+}
+
+# ===== print usage =====
+function print_usage() {
+    echo "Usage: $PROGRAM [OPTION]"
+    echo "  -h, --help, -help"
+    echo "      print manual"
+    echo "  -v <new_version>, --version <new_version>"
+    echo "      update with specific version"
+}
+
+function usage_and_exit() {
+    print_usage
+    exit "$1"
+}
+
+# ======================
+# parse arguments (options)
+# ======================
+for i in "$@"; do
+    case $i in
+    -h | --help | -help)
+        usage_and_exit 0
+        ;;
+    -v | --version)
+        if [[ -z "$2" ]]; then
+            print_error "option requires a new version for $1 option"
+            echo ""
+            usage_and_exit 1
+        fi
+        echo "$2"
+        if [[ "$2" =~ ^([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
+            NEW_VERSION="$2"
+        else
+            print_error "new version format should be \d\.\d\.\d for $1 option"
+            echo ""
+            usage_and_exit 1
+        fi
+        
+        shift 2
+        ;;
+    -*)
+        echo "Unknown option $1"
+        usage_and_exit 1
+        ;;
+    esac
+done
+
+PROGRAM="$(basename "$0")"
 FILE_NAME="TwitterClone.xcodeproj/project.pbxproj"
 BACKUP_FILE_NAME="${FILE_NAME}.${PROGRAM}.backup"
 # make a backup just in case
@@ -57,14 +112,18 @@ elif [[ "${IS_MINOR_UPDATE}" == "true" ]]; then
 else
     NEXT_VERSION="$(echo "${CURRENT_VERSION}" | awk -F'.' -v OFS='.' '{print $1, $2, ($3+1)}' )"
 fi
-echo "NEW version: $NEXT_VERSION"
+
+if [ -z "${NEW_VERSION}" ]; then
+    NEW_VERSION="${NEXT_VERSION}"
+fi
+echo "NEW version: $NEW_VERSION"
 
 sed -i -e \
-    "s@MARKETING_VERSION = .*;@MARKETING_VERSION = "${NEXT_VERSION}";@g" \
+    "s@MARKETING_VERSION = .*;@MARKETING_VERSION = "${NEW_VERSION}";@g" \
     "${FILE_NAME}"
 
 # e.g) CURRENT_PROJECT_VERSION: 10001 <- 1*10000 + 0*100 + 1
-CURRENT_PROJECT_VERSION="$(echo "${NEXT_VERSION}" | awk -F'.' '{print $1*10000 + $2*100 + $3}')"
+CURRENT_PROJECT_VERSION="$(echo "${NEW_VERSION}" | awk -F'.' '{print $1*10000 + $2*100 + $3}')"
 sed -i -e \
     "s@CURRENT_PROJECT_VERSION = .*;@CURRENT_PROJECT_VERSION = "${CURRENT_PROJECT_VERSION}";@g" \
     "${FILE_NAME}"
